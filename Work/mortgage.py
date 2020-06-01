9c6be1aba1b76710b28bcb7bda8cf9d590f30ff9
Bodo Brueckner <brueckner.bodo@googlemail.com>
2020-06-01 10:45:59 +0200

    Create enhanced version of mortgage program

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,13 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month = 0
 
 while principal > 0:
     principal = principal * (1 + rate/12) - payment
     total_paid = total_paid + payment
+    month = month + 1
+    if month % 12 == 0:
+        print(f'Sum paid after {month} months is {round(total_paid, 2)}')
 
-print('Total paid', total_paid)
+print(f'Total paid is {total_paid} or rounded to two decimals {round(total_paid, 2)}')
